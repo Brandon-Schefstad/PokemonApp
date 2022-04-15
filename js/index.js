a1803eacac86c530088cc170eff8602f942d236b
Brandon-Schefstad <bschefstad@gmail.com>
2022-04-15 10:18:17 -0400

    Set up environmental variables, messages work

---
+++ js/index.js
@@ -0,0 +1,29 @@
+import Vonage from '@vonage/server-sdk';
+import dotenv from 'dotenv/config';
+
+const vonage = new Vonage({
+	apiKey: process.env.API_KEY,
+	apiSecret: process.env.APISECRET,
+});
+
+// function sendSMS() {
+// 	console.log(`sending SMS`);
+// 	const from = '18776934395';
+// 	const to = '14077387133';
+// 	const text = 'Testing from VSCode!';
+// 	vonage.message.sendSms(from, to, text, (err, responseData) => {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			if (responseData.messages[0]['status'] === '0') {
+// 				console.log('Message sent successfully.');
+// 			} else {
+// 				console.log(
+// 					`Message failed with error: ${responseData.messages[0]['error-text']}`
+// 				);
+// 			}
+// 		}
+// 	});
+// }
+
+// sendSMS();
