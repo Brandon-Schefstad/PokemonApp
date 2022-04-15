a5de1dbbe15d4bfc5f61cfe713b531037f409236
Brandon-Schefstad <bschefstad@gmail.com>
2022-04-15 10:25:44 -0400

    Added frontend interface

---
+++ dist/js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('button').addEventListener('click', sendSMS);
+
+function sendSMS() {
+	alert(`Text!`);
+}
--- js/index.js
+++ js/index.js
@@ -6,24 +6,24 @@ const vonage = new Vonage({
 	apiSecret: process.env.APISECRET,
 });
 
-// function sendSMS() {
-// 	console.log(`sending SMS`);
-// 	const from = '18776934395';
-// 	const to = '14077387133';
-// 	const text = 'Testing from VSCode!';
-// 	vonage.message.sendSms(from, to, text, (err, responseData) => {
-// 		if (err) {
-// 			console.log(err);
-// 		} else {
-// 			if (responseData.messages[0]['status'] === '0') {
-// 				console.log('Message sent successfully.');
-// 			} else {
-// 				console.log(
-// 					`Message failed with error: ${responseData.messages[0]['error-text']}`
-// 				);
-// 			}
-// 		}
-// 	});
-// }
+function sendSMS() {
+	console.log(`sending SMS`);
+	const from = '18776934395';
+	const to = '14077387133';
+	const text = 'Testing from VSCode!';
+	vonage.message.sendSms(from, to, text, (err, responseData) => {
+		if (err) {
+			console.log(err);
+		} else {
+			if (responseData.messages[0]['status'] === '0') {
+				console.log('Message sent successfully.');
+			} else {
+				console.log(
+					`Message failed with error: ${responseData.messages[0]['error-text']}`
+				);
+			}
+		}
+	});
+}
 
 // sendSMS();
